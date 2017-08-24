a3a668e977739d59fd1df9a9a786c1cc73277a12
Alina Stolbikova <a.stolbikova@module.ru>
2017-08-24 17:10:05 +0300

    Have been fixed fw library for MPW7705

--- lib/fw_mm7705.lua
+++ lib/fw_mm7705.lua
@@ -1,11 +1,13 @@
 --Params are specified to MM7705
 local fw = {}
 
-local bootheader_magic_host_image_valid = 0xdeadc0de
+--local bootheader_magic_host_image_valid = 0xdeadc0de
+local bootheader_magic_host_image_valid = 0xdec0adde
 local im0 = 0x00040000
 
 fw.run_code = function(file, slavemode)
-  if edcl_upload(im0, file) then
+  --if edcl_upload(im0, file) then
+  if (-1 != edcl_upload(im0, file)) then
     edcl_write(4, im0, bootheader_magic_host_image_valid)
   else
     error("Failed to upload file or no file found!")
