48e75e13b0aedde1911dc72badcbc024e8d8c42d
Alina Stolbikova <a.stolbikova@module.ru>
2018-03-28 14:48:38 +0300

    Fixed error in lua script.

--- lib/fw_mm7705.lua
+++ lib/fw_mm7705.lua
@@ -7,7 +7,7 @@ local im0 = 0x00040000
 
 fw.run_code = function(file, slavemode)
   --if edcl_upload(im0, file) then
-  if (-1 != edcl_upload(im0, file)) then
+  if (-1 ~= edcl_upload(im0, file)) then
     edcl_write(4, im0, bootheader_magic_host_image_valid)
   else
     error("Failed to upload file or no file found!")
